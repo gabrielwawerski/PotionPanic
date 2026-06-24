109e510f91216ce01d9196f3c9ed6f90dd157b3e
patronas <patrykblaszczak1@gmail.com>
2026-06-24 20:30:18 +0100

    test

--- test.cs
+++ test.cs
@@ -3,8 +3,10 @@ namespace DefaultNamespace;
 public class test
 {
   # test
-  # kurwa tescik
-    # tescik kolejny
+  # kurwa tescik piekny
+  # tescik kolejny niezbyt piekny
+    # tescik kolejny niezbyt piekny
+
 }
 
 pierdol sie kurwa! nie bawimy sie tak kurwa!
