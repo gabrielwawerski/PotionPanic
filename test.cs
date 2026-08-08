d4be042e4892636d92822a7802ce3dfc33d0fe7e
patronas <patrykblaszczak1@gmail.com>
2026-08-08 18:28:25 +0100

    vsc test commit

--- test.cs
+++ test.cs
@@ -2,11 +2,8 @@ namespace DefaultNamespace;
 
 public class test
 {
-  # test
-  # kurwa tescik piekny
-  # tescik kolejny niezbyt piekny
-    # tescik kolejny niezbyt piekny
+  #test
 
 }
 
-pierdol sie kurwa! nie bawimy sie tak kurwa!
+
